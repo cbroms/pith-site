fad0a9bf39a9ee23475d187c5efb64a73fa1cbde
Christian Broms <cb@christianbroms.com>
2020-11-28 17:47:00 -0800

    fix styled components to render statically

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9b22373716e3aa172009";
+/******/ 	var hotCurrentHash = "a24e535de033723a3508";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
