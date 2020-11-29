11373b30ef08e02b5272af39760e04862cb7b9d9
Christian Broms <cb@christianbroms.com>
2020-11-28 17:53:38 -0800

    global styled with styled components at app root

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
-import "../styles/global.css";
+import { createGlobalStyle } from "styled-components";
+
+import React from "react";
+
+const GlobalStyle = createGlobalStyle`
+ @import url("https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600&family=Source+Serif+Pro:ital,wght@0,400;0,600;1,400&display=swap");
+
+body {
+	font-family: "Source Serif Pro", serif;
+	margin: 0;
+	font-weight: 300;
+}
+
+html {
+	font-size: 16px;
+}
+
+`;
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<React.Fragment>
+			<GlobalStyle />
+			<Component {...pageProps} />
+		</React.Fragment>
+	);
 }
 
 export default MyApp;
